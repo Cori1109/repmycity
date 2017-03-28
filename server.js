58039198dfa033db31234e2ac41404e3c385a45f
Cori1109 <chris.jenkins.j1005@gmail.com>
2017-03-28 14:29:48 -0600

    bugfix script

--- server.js
+++ server.js
@@ -1,24 +1,40 @@
-const path = require('path');
 var express = require('express');
-
-// Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
 
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/public/index.html');
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('*', function(req, res) {
-  response.render('public/index.html');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-app.listen(PORT, function () {
-  console.log('Express server is up on port ' + PORT);
-});
+
+// const path = require('path');
+// var express = require('express');
+//
+// // Create our app
+// var app = express();
+// const PORT = process.env.PORT || 3000;
+//
+// app.use(function (req, res, next){
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     res.redirect('http://' + req.hostname + req.url);
+//   } else {
+//     next();
+//   }
+// });
+//
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.get('*', function(req, res) {
+//   response.render('public/index.html');
+// });
+//
+// app.listen(PORT, function () {
+//   console.log('Express server is up on port ' + PORT);
+// });
